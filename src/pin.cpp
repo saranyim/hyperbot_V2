6af557793637cf3da695adccd9b44d15dbbd8481
AUTARO1 <napatkinz@gmail.com>
2026-01-03 18:00:29 +0700

    Flip over fine tune

--- src/pin.cpp
+++ src/pin.cpp
@@ -173,6 +173,11 @@ void Flip_Pin_Over() {
     mg_pin.setVelocity(100.0, percent);
     mg_pin.spin(forward);
     wait(0.4, seconds);
+    mg_beam.setMaxTorque(100, percent);
+    mg_beam.setVelocity(100, percent);
+    mg_beam.setStopping(hold);
+    mg_beam.spinFor(reverse, 250, degrees, false);
+    
 
 
     while(mg_pin.velocity(percent) > 30.0) {
@@ -181,10 +186,6 @@ void Flip_Pin_Over() {
     mg_pin.stop();
 
     pinPos = bottom;
-    mg_beam.setMaxTorque(100, percent);
-    mg_beam.setVelocity(100, percent);
-    mg_beam.setStopping(hold);
-    mg_beam.spinFor(reverse, 250, degrees, false);
     
 
 }
