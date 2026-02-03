81695e1f01680fed9323a2fee18d8b96bb204bb1
AUTARO1 <napatkinz@gmail.com>
2026-02-03 16:46:54 +0700

    merge

--- src/drive.cpp
+++ src/drive.cpp
@@ -16,16 +16,10 @@ int TaskDriveTrain() {
             TouchLED12.setColor(green);
             mot_dtRight.setVelocity((Controller.AxisA.position() - Controller.AxisB.position()), percent);
             mot_dtLeft.setVelocity((Controller.AxisA.position() + Controller.AxisB.position()), percent);
-<<<<<<< HEAD
+
             mot_dtLeft.spin(reverse);
             mot_dtRight.spin(reverse);
-=======
-            mot_dtLeft.spin(forward);
-            mot_dtRight.spin(forward);
-            mot_dtLeft.spin(forward);
-            mot_dtRight.spin(forward);
->>>>>>> 80f7cb49801084c6fa1771225010f67eb2b20263
-        }
+       }
         else {
             TouchLED12.setColor(orange);
             mot_dtRight.setVelocity((Controller.AxisA.position() + Controller.AxisB.position()), percent);
